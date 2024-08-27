2bfedcc30bf388659ad9c0e34d8fa688c2b4e9c3
Raymond Oluoch <rasholuoch@gmail.com>
2024-08-27 18:14:15 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fullstack Inventory Management Dashboard
+
+This is a full stack application for engineer
+
+## Tech Stack
+
+- **Next JS**
+- **Tailwind**
+- **Redux Toolkit**
+- **Redux Toolkit Query**
+- **Material UI Data Grid**
+- **Node.js**
+- **Prisma**
+- **AWS EC2**
+- **AWS RDS**
+- **AWS API Gateway**
+- **AWS Amplify**
+- **AWS S3**
---
+++ client/src/app/layout.tsx
@@ -0,0 +1,26 @@
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Inventory Management Dashboard",
+  description: "A full stack application for managing inventory",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <DashboardWrapper>{children}</DashboardWrapper>
+      </body>
+    </html>
+  );
+}
